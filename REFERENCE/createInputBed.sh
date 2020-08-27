7013ed45f7e81dd30b1b51036a8fc52bcd0f456c
Cathal Ó Ruaidh <32680817+cathaloruaidh@users.noreply.github.com>
2020-08-27 15:03:23 +0100

    Update createInputBed.sh

--- REFERENCE/createInputBed.sh
+++ REFERENCE/createInputBed.sh
@@ -4,14 +4,11 @@ CHR=${1};
 START=${2};  
 END=${3}; 
 
-while IFS="" read -r LINE || [[ -n "${LINE}" ]]
-do
-	mkdir FASTA_BED.${CHR} ; 
+mkdir FASTA_BED.${CHR} ; 
 
-	time for (( i=${START}; i<${END}; i++ ))
-	do 
-		echo -e "${CHR}\t${i}\t$((i+1))\tFASTA_BED_${CHR}_${i}" ; 
-	done > FASTA_BED.${CHR}/FASTA_BED.${CHR}.bed ; 
+time for (( i=${START}; i<${END}; i++ ))
+do 
+	echo -e "${CHR}\t${i}\t$((i+1))\tFASTA_BED_${CHR}_${i}" ; 
+done > FASTA_BED.${CHR}/FASTA_BED.${CHR}.bed ; 
 
-	echo ${CHR} ; 
-done 
+echo ${CHR} ; 
